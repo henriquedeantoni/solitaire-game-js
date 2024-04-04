4008a19c7dd3d7101140f1a5723859679133dbe8
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-04-04 12:46:26 -0300

    Update and rename shiftCardColumn.js to shiftLastCardColumn.js

--- shiftLastCardColumn.js
+++ shiftLastCardColumn.js
@@ -1,6 +1,6 @@
 // Muda a posição da carta do topo para o fundo
 
-function shiftCardColumn(cardList, n){
+function shiftLastCardColumn(cardList, n){
   const tempCard = cardList[n];
   cardList[n] = cardList[0]
   cardList[0] = temp
