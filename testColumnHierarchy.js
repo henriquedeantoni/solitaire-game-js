82b35747b3ee424bff3c6debffa1f32aabe8e36e
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-04-05 13:03:00 -0300

    Update testColumnHierarchy.js

--- testColumnHierarchy.js
+++ testColumnHierarchy.js
@@ -1,7 +1,7 @@
 function testColumnHierarchy(list){
     let isSortedAce=true
     list.forEach((card, index) =>{
-        if(card[index].value<card[index+1].value && card[index+1]){
+        if(card[index].value<card[index+1].value && index+1<list.length){
             isSortedAce=true
         }
         isSortedAce=false
