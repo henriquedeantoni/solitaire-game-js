a5dfcda079d4f815ae29db0f15c846dc61897c33
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-04-11 12:47:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+
+
+
+
+/*
+
 const columns = document.querySelectorAll(".column");
 
 document.addEventListener("dragstart", (element)=>{
@@ -36,3 +56,6 @@ function getNewPosition(column, posY){
     }
     return result;
 }
+
+
+*/
